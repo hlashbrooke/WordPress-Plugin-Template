011294a15d233e0de592d98c4a12b111e35973a5
Carl Alberto <carl.alberto@getpantheon.com>
2019-04-22 02:40:07 +0800

    cleanup uninstall.php file

--- uninstall.php
+++ uninstall.php
@@ -1,15 +1,14 @@
 <?php
-
 /**
- * 
  * This file runs when the plugin in uninstalled (deleted).
  * This will not run when the plugin is deactivated.
  * Ideally you will add all your clean-up scripts here
  * that will clean-up unused meta, options, etc. in the database.
  *
+ * @package WordPress Plugin Template/Uninstall
  */
 
-// If plugin is not being uninstalled, exit (do nothing)
+// If plugin is not being uninstalled, exit (do nothing).
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
